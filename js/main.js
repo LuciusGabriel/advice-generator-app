2ba83ffe96e451ec4e516643e2dcb3c5454887ec
LuciusGabriel <lucius_gabriel@hotmail.com>
2022-10-30 22:10:20 -0300

    Adciona Script Fetch API

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+const fetchAdvice = async () => {
+    const resultado = await fetch("https://api.adviceslip.com/advice")
+    const data = await resultado.json()
+
+    const idAdvice = data.slip.id
+    const advice = data.slip.advice
+    document.getElementById('id-advice').innerHTML = `Advice: ${idAdvice}`
+    document.getElementById('advice').innerHTML = `${advice}`
+}
+
+fetchAdvice()
