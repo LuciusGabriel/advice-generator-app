c0038ee973daf17c3da7cb36dcdd94f98cd1aca5
LuciusGabriel <lucius_gabriel@hotmail.com>
2022-11-04 11:37:48 -0300

    Entrega

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const fetchAdvice = async () => {
+    const resultado = await fetch("https://api.adviceslip.com/advice")
+    const data = await resultado.json()
+
+    const idAdvice = data.slip.id
+    const advice = data.slip.advice
+    document.querySelector('.id-advice').innerHTML = `Advice # ${idAdvice}`
+    document.querySelector('.advice').innerHTML = `"${advice}"`
+}
+
+fetchAdvice()
